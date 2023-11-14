e2768ee9550fd183124aed6ad3622bc222998158
Sieglinde <sieglindevwb@gmail.com>
2023-11-14 11:58:12 +0100

    Add data to the input

--- script.js
+++ script.js
@@ -11,55 +11,92 @@
 // })
 //   .then((response) => response.json())
 //   .then((json) => console.log(json));
-let geoLocation = ('https://geocoding-api.open-meteo.com/v1/search');
+const button = document.querySelector(".submitCityName");
+const inputField = document.querySelector('#cityName');
 
-async function getGeoLocation(){
-    // Use fetch to make the API request
-    let response = await fetch(geoLocation);
-
-    // Use response.json() to parse the response body as JSON
-    let data = await response.json();
-
-    // Now you can work with the parsed JSON datav
-    console.log(data);
+async function getGeoData(cityName){
+    const response = await fetch('https://geocoding-api.open-meteo.com/v1/search?name=' + cityName + '&count=10&language=en&format=json');
+    const data = await response.json();
+    return data.results[0];
 }
 
-function createFormWithInputAndButton(labelText, buttonLabel) {
-    //create form
-    let form = document.createElement("form");
+async function getWeatherData(location) {
+    const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude='+ location.latitude +'&longitude='+ location.longitude)
+    const data = await response.json();
+    return data;
+}
 
-    //create h1 element
-    let label = document.createElement("h1");
-    label.textContent = labelText;
-    form.appendChild(label);
-    console.log("label:", labelText);
-    
-    // Create input element
-    let input = document.createElement("input");
-    input.setAttribute("type", "text");
-    form.appendChild(input);
-    console.log("input created");
+async function startWeatherApp(){
+    const getCityInput = document.querySelector('#cityName').value;
 
-    // create submit button
-    let submitButton = document.createElement("button")
-    submitButton.setAttribute("type", "submit");
-    submitButton.textContent = buttonLabel || "Submit";
-    form.appendChild(submitButton);
+    const geo = await getGeoData(getCityInput);
 
-    document.body.appendChild(form);
+    // Check if geo-location data is available
+    if (geo) {
+        const weather = await getWeatherData(geo);
 
-    form.addEventListener("submit", (event) => {
+         // Log the entire weather object to inspect its structure
+         console.log('Full Weather Data:', weather);
+
+        const currentTemperature = weather.current_weather.temperature;
+        const conditions = weather.current_weather.conditions;
+        const description = weather.current_weather.description; 
+        // Now you can work with the weather data
+        console.log('current temperature: ' + currentTemperature + '°C');
+        console.log('conditions: ' + conditions);
+        console.log('Description: ' + description);
+    } else {
+        console.error('Error fetching geo-location data');
+    }
+}
+
+inputField.addEventListener('keyup', (event) => {
+    if (event.code === "Enter") {
         event.preventDefault();
-        console.log("City input created:", input.value);
-    });
-    
- // Return the input element in case you want to do further operations with it
-return form;
-}
+        startWeatherApp()
+    }
+})
+button.addEventListener('click', (event) => {
+    event.preventDefault();
+    startWeatherApp();
+})
 
-// Example usage
-let form = createFormWithInputAndButton("Choose city");
-console.log("City input created:", form);
+// function createFormWithInputAndButton(labelText, buttonLabel) {
+//     //create form
+//     let form = document.createElement("form");
+
+//     //create h1 element
+//     let label = document.createElement("h1");
+//     label.textContent = labelText;
+//     form.appendChild(label);
+//     console.log("label:", labelText);
+    
+//     // Create input element
+//     let input = document.createElement("input");
+//     input.setAttribute("type", "text");
+//     form.appendChild(input);
+//     console.log("input created");
+
+//     // create submit button
+//     let submitButton = document.createElement("button")
+//     submitButton.setAttribute("type", "submit");
+//     submitButton.textContent = buttonLabel || "Submit";
+//     form.appendChild(submitButton);
+
+//     document.body.appendChild(form);
+
+//     form.addEventListener("submit", (event) => {
+//         event.preventDefault();
+//         console.log("City input created:", input.value);
+//     });
+    
+//  // Return the input element in case you want to do further operations with it
+// return form;
+// }
+
+// // Example usage
+// let form = createFormWithInputAndButton("Choose city");
+// console.log("City input created:", form);
 
 
 // TODO: Use an api to get the weather data for at least the next 5 days
