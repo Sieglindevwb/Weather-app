90376ce033a64dbd3be1bb1f5f4747fa8f707948
Sieglinde <sieglindevwb@gmail.com>
2023-11-17 11:59:38 +0100

    cleaning up the code

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-// fetch('https://jsonplaceholder.typicode.com/posts', {
-//     method:'POST',
-//     body: JSON.stringify({
-//         title: 'foo',
-//         body: 'bar',
-//         userId: 1,
-//     }),
-//     headers: {
-//         'content-type': 'application/json; charset=UTF-8',
-//     },
-// })
-//   .then((response) => response.json())
-//   .then((json) => console.log(json));
 const button = document.querySelector(".submitCityName");
 const inputField = document.querySelector('#cityName');
 
 async function getGeoData(cityName){
-    const response = await fetch('https://geocoding-api.open-meteo.com/v1/search?name=' + cityName + '&count=10&language=en&format=json');
-    const data = await response.json();
-    const cityNameElement = document.querySelector('#city');
-    
-    cityNameElement.textContent = data.results[0].name + ', ' + data.results[0].country;
-    console.log(data);
-    console.log(data.results[0].country);
-    return data.results[0];
+    try {
+        const response = await fetch('https://geocoding-api.open-meteo.com/v1/search?name=' + cityName + '&count=10&language=en&format=json');
+        const data = await response.json();
+        const cityNameElement = document.querySelector('#city');
+        
+        cityNameElement.textContent = data.results[0].name + ', ' + data.results[0].country;
+        console.log(data);
+        console.log(data.results[0].country);
+        return data.results[0];
+    }
+    catch (error){
+        console.error('Error fetching geo-location data:', error);
+        return null;
+    }
 }
 
 async function getWeatherData(location) {
-    const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude='+ location.latitude +'&longitude='+ location.longitude + '&current=temperature_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code&hourly=temperature_2m,relative_humidity_2m,precipitation_probability,precipitation,rain,showers,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_sum&timezone=Europe%2FLondon&forecast_days=3');
-    const data = await response.json();
-    console.log('Weather Data Response:', data);
-    return data;
+    try {
+        const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude='+ location.latitude +'&longitude='+ location.longitude + '&current=temperature_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code&hourly=temperature_2m,relative_humidity_2m,precipitation_probability,precipitation,rain,showers,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_sum&timezone=Europe%2FLondon&forecast_days=3');
+        const data = await response.json();
+        console.log('Weather Data Response:', data);
+        return data;
+    }
+    catch (error){
+        console.error('Error fetching weather data:', error);
+        return null;
+    }
 }
 
 function updateWeatherDisplay(weather) {
@@ -60,21 +59,25 @@ function updateWeatherDisplay(weather) {
     dailyForecastData.time.forEach((day, index) => {
         const date = new Date(day);
         // Inside the loop where you create daily forecast items
-        const dayElement = document.createElement('div');
+        const dayElement = document.createElement('section');
         dayElement.classList.add('daily-forecast-item');
 
         // Create separate paragraphs for each piece of information
         const dateParagraph = document.createElement('p');
         dateParagraph.textContent = date.toDateString();
+        dateParagraph.classList.add('date');
 
         const maxTemperatureParagraph = document.createElement('p');
         maxTemperatureParagraph.textContent = 'Max ' + dailyForecastData.temperature_2m_max[index] + '°C';
+        maxTemperatureParagraph.classList.add('max-temperature');
 
         const minTemperatureParagraph = document.createElement('p');
         minTemperatureParagraph.textContent = 'Min ' + dailyForecastData.temperature_2m_min[index] + '°C';
+        minTemperatureParagraph.classList.add('min-temperature');
 
         const precipitationParagraph = document.createElement('p');
         precipitationParagraph.textContent = 'Precipitation ' + dailyForecastData.precipitation_sum[index] + 'mm';
+        precipitationParagraph.classList.add('precipitation');
 
         // Create an img element for the daily forecast icon
         const iconElement = document.createElement('img');
@@ -106,6 +109,7 @@ const descriptions = {
     60: 'Light rain shower',
     61: 'Rain shower',
     63: 'Heavy rain shower',
+    71: 'Snow fall'
 };
 
 function getWeatherDescription(weatherCode) {
@@ -136,6 +140,8 @@ function getWeatherIcon(weatherCode, isCurrentWeather = true) {
             return './icons/heavyRain.gif';
         case 'Heavy rain shower':
             return './icons/heavyRain.gif';
+        case 'Snow fall':
+            return './snow.gif';
         default:
             return './icons/cloud.gif';
     }
@@ -190,42 +196,6 @@ button.addEventListener('click', (event) => {
     startWeatherApp();
 })
 
-// function createFormWithInputAndButton(labelText, buttonLabel) {
-//     //create form
-//     let form = document.createElement("form");
-
-//     //create h1 element
-//     let label = document.createElement("h1");
-//     label.textContent = labelText;
-//     form.appendChild(label);
-//     console.log("label:", labelText);
-    
-//     // Create input element
-//     let input = document.createElement("input");
-//     input.setAttribute("type", "text");
-//     form.appendChild(input);
-//     console.log("input created");
-
-//     // create submit button
-//     let submitButton = document.createElement("button")
-//     submitButton.setAttribute("type", "submit");
-//     submitButton.textContent = buttonLabel || "Submit";
-//     form.appendChild(submitButton);
-
-//     document.body.appendChild(form);
-
-//     form.addEventListener("submit", (event) => {
-//         event.preventDefault();
-//         console.log("City input created:", input.value);
-//     });
-    
-//  // Return the input element in case you want to do further operations with it
-// return form;
-// }
-
-// // Example usage
-// let form = createFormWithInputAndButton("Choose city");
-// console.log("City input created:", form);
 
 
 // TODO: Use an api to get the weather data for at least the next 5 days
